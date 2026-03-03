0f1f808abe8d8ce0d400952e7d8509b8f23ade36
Wonjae Song <stojum@naver.com>
2026-03-03 16:27:31 +0900

    step1 : Pawn 구현

---
+++ ChessGame/src/test/java/PawnTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class PawnTest {
+
+    @Test
+    @DisplayName("흰색과 검은색 폰이 올바르게 생성되어야 한다")
+    public void create() {
+
+        String white = "white";
+        String black = "black";
+
+        verifyPawn(white);
+        verifyPawn(black);
+    }
+
+    private void verifyPawn(String color) {
+        Pawn pawn = new Pawn(color);
+        assertThat(pawn.getColor()).isEqualTo(color);
+    }
+
+    @Test
+    @DisplayName("매개변수 없이 생성하면 흰색 폰이 생성되어야 한다")
+    public void create_default() {
+        Pawn pawn = new Pawn();
+        assertThat(pawn.getColor()).isEqualTo("white");
+    }
+}
