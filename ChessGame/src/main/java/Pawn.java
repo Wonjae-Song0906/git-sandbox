7585fc14b047f83710313d10b3dbcb96bea493d2
Wonjae Song <stojum@naver.com>
2026-03-03 16:24:32 +0900

    Pawn 클래스 구현

---
+++ ChessGame/src/main/java/Pawn.java
@@ -0,0 +1,15 @@
+public class Pawn {
+    private final String color;
+
+    public Pawn() {
+        this.color = "white";
+    }
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
